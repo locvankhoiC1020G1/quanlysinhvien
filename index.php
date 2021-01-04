81b23b1fa10e324cf2e4f6f660830b671237ffba
locvankhoiC1020G1 <locvankhoi@gmail.com>
2021-01-04 10:52:42 +0700

    taobang

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quản lý sinh viên</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+<h1>Quản lý sinh vien</h1>
+<table>
+
+    <tr>
+        <td>Tên SV</td>
+        <td>Tuổi</td>
+        <td>Lớp</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+<form>
+</form>
+</body>
+</html>
+
+
+<?php
+
+?>
